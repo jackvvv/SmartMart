709a1a0485dfb67818fb2139c8bf23836a234561
jackvvv <954828748@qq.com>
2016-09-14 11:15:04 +0800

    试试

--- app/src/main/java/sinia/com/smartmart/fragment/HomeFragment.java
+++ app/src/main/java/sinia/com/smartmart/fragment/HomeFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -14,8 +15,6 @@ import android.widget.TextView;
 import com.ToxicBakery.viewpager.transforms.ABaseTransformer;
 import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
-import com.cpoopc.scrollablelayoutlib.ScrollableHelper;
-import com.cpoopc.scrollablelayoutlib.ScrollableLayout;
 
 import java.util.ArrayList;
 
@@ -28,49 +27,35 @@ import sinia.com.smartmart.adapter.HomeAdapter;
 import sinia.com.smartmart.base.BaseFragment;
 import sinia.com.smartmart.utils.AppInfoUtil;
 import sinia.com.smartmart.utils.DialogUtils;
-import sinia.com.smartmart.utils.Utility;
 import sinia.com.smartmart.view.LocalImageHolderView;
 
 /**
  * Created by 忧郁的眼神 on 2016/9/3.
  */
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseFragment implements View.OnClickListener {
 
     @Bind(R.id.imgSearch)
     ImageView imgSearch;
-    @Bind(R.id.tv_address)
-    TextView tvAddress;
-    @Bind(R.id.convenientBanner)
-    ConvenientBanner convenientBanner;
-    @Bind(R.id.img)
-    ImageView img;
-    @Bind(R.id.imgFood)
-    ImageView imgFood;
-    @Bind(R.id.imgFarm)
-    ImageView imgFarm;
-    @Bind(R.id.imgJancai)
-    ImageView imgJancai;
-    @Bind(R.id.imgMore)
-    ImageView imgMore;
-    @Bind(R.id.img_jiazheng)
-    ImageView imgJiazheng;
-    @Bind(R.id.imgWash)
-    ImageView imgWash;
-    @Bind(R.id.imgClean)
-    ImageView imgClean;
-    @Bind(R.id.img_all)
-    ImageView img_all;
-    @Bind(R.id.img_smart)
-    ImageView img_smart;
-    @Bind(R.id.tv_all)
-    TextView tvAll;
-    @Bind(R.id.tv_smart)
-    TextView tvSmart;
     @Bind(R.id.listView)
     ListView listView;
-    private View rootView;
+    @Bind(R.id.tv_all)
+    TextView tvAll;
+    @Bind(R.id.img_all)
+    ImageView imgAll;
+    @Bind(R.id.tv_smart)
+    TextView tvSmart;
+    @Bind(R.id.img_smart)
+    ImageView imgSmart;
+    @Bind(R.id.invis)
+    LinearLayout invis;
+
+    private TextView tvAddress;
+    private LinearLayout ll_all, ll_smart;
+    private ImageView imgFood, imgClean, imgWash, imgMore, imgJancai, imgFarm;
+    private View rootView, headerView, stickyFilterView;
     private HomeAdapter adapter;
     private ArrayList<Integer> localImages = new ArrayList<Integer>();
+    private ConvenientBanner convenientBanner;
 
     @Nullable
     @Override
@@ -78,6 +63,7 @@ public class HomeFragment extends BaseFragment {
     Bundle savedInstanceState) {
         rootView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home, null);
         ButterKnife.bind(this, rootView);
+        initView();
         initData();
         return rootView;
     }
@@ -107,11 +93,54 @@ public class HomeFragment extends BaseFragment {
 
         adapter = new HomeAdapter(getActivity());
         listView.setAdapter(adapter);
-        Utility.setListViewHeightBasedOnChildren(listView);
     }
 
-    @OnClick({R.id.imgSearch, R.id.tv_address, R.id.imgFood, R.id.imgFarm, R.id.imgJancai, R.id.imgMore, R.id
-            .img_jiazheng, R.id.imgWash, R.id.imgClean, R.id.tv_all, R.id.tv_smart})
+    private void initView() {
+        headerView = View.inflate(getActivity(), R.layout.view_home_head, null);
+        stickyFilterView = View.inflate(getActivity(), R.layout.view_home_sticky_filter, null);
+        convenientBanner = (ConvenientBanner) headerView.findViewById(R.id.convenientBanner);
+        tvAddress = (TextView) headerView.findViewById(R.id.tv_address);
+        imgFood = (ImageView) headerView.findViewById(R.id.imgFood);
+        imgFarm = (ImageView) headerView.findViewById(R.id.imgFarm);
+        imgJancai = (ImageView) headerView.findViewById(R.id.imgJancai);
+        imgMore = (ImageView) headerView.findViewById(R.id.imgMore);
+        imgWash = (ImageView) headerView.findViewById(R.id.imgWash);
+        imgClean = (ImageView) headerView.findViewById(R.id.imgClean);
+        ll_smart = (LinearLayout) stickyFilterView.findViewById(R.id.ll_smart);
+        ll_all = (LinearLayout) stickyFilterView.findViewById(R.id.ll_all);
+        setClickListener();
+        listView.addHeaderView(headerView);
+        listView.addHeaderView(stickyFilterView);
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem >= 1) {
+                    invis.setVisibility(View.VISIBLE);
+                } else {
+                    invis.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private void setClickListener() {
+        imgFood.setOnClickListener(this);
+        imgFarm.setOnClickListener(this);
+        imgJancai.setOnClickListener(this);
+        imgMore.setOnClickListener(this);
+        imgWash.setOnClickListener(this);
+        imgClean.setOnClickListener(this);
+        ll_smart.setOnClickListener(this);
+        ll_all.setOnClickListener(this);
+    }
+
+    @OnClick({R.id.imgSearch, R.id.llAll, R.id.llSmart})
     public void onClick(View view) {
         Intent intent = null;
         switch (view.getId()) {
@@ -119,7 +148,17 @@ public class HomeFragment extends BaseFragment {
                 intent = new Intent(getActivity(), SearchActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.tv_address:
+            case R.id.llAll:
+                showToast("1");
+                break;
+            case R.id.llSmart:
+                showToast("2");
+                break;
+            case R.id.ll_smart:
+                showToast("22");
+                break;
+            case R.id.ll_all:
+                showToast("11");
                 break;
             case R.id.imgFood:
                 break;
@@ -137,10 +176,6 @@ public class HomeFragment extends BaseFragment {
                 break;
             case R.id.imgClean:
                 DialogUtils.createFountionDevelopingTipsDialog(getActivity(), "清洁服务正在完善中...");
-                break;
-            case R.id.tv_all:
-                break;
-            case R.id.tv_smart:
                 break;
         }
     }
